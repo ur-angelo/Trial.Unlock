c50c9731e4c30600aa4e8cc759dbf94f150de889
! angelo 𓂀 <ur.angelo97@gmail.com>
2026-06-28 17:45:27 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,16 +1,37 @@
 Clear-Host
 
-Write-Host "====================================" -ForegroundColor Red
-Write-Host "        تم تحديث النظام / CMD        " -ForegroundColor Red
-Write-Host " الرجاء التوجه إلى سيرفر Discord " -ForegroundColor Red
-Write-Host " للحصول على النسخة الجديدة من CMD " -ForegroundColor Red
-Write-Host "====================================" -ForegroundColor Red
+Write-Host @"
 
-$Discord = "https://discord.gg/ضع-الرابط-هنا"
+                                       ██████╗  ██████╗     ██╗ █████╗ ███╗   ██╗ ██████╗     
+                                      ██╔════╝ ██╔════╝    ██╔╝██╔══██╗████╗  ██║██╔════╝     
+                                      ██║  ███╗██║  ███╗  ██╔╝ ███████║██╔██╗ ██║██║  ███╗    
+                                      ██║   ██║██║   ██║ ██╔╝  ██╔══██║██║╚██╗██║██║   ██║    
+                                   ██╗╚██████╔╝╚██████╔╝██╔╝   ██║  ██║██║ ╚████║╚██████╔╝    
+                                   ╚═╝ ╚═════╝  ╚═════╝ ╚═╝    ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝     
+                                                            
+
+                                                     made by @angelo x
+
+"@ -ForegroundColor Blue
+
+Start-Sleep -Seconds 3
+
+
+                                   Write-Host "====================================" -ForegroundColor Red
+                                   Write-Host "        CMD HAS BEEN UPDATED        " -ForegroundColor Red
+                                   Write-Host " Please join the Discord server     " -ForegroundColor Red
+                                   Write-Host " to get the new CMD version         " -ForegroundColor Red
+                                   Write-Host "====================================" -ForegroundColor Red
+
+$Website = "https://guns.lol/angelo_x"
+
+$Discord = "https://discord.gg/NS5QHe2StG"
 
 Write-Host ""
-Write-Host "فتح رابط Discord..." -ForegroundColor Yellow
+Write-Host "Opening website..." -ForegroundColor Yellow
+Start-Process $Website
 
+Write-Host "Opening Discord server..." -ForegroundColor Yellow
 Start-Process $Discord
 
 Pause
