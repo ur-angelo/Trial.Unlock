66498eeafc20be743a9cf8cef8528938edd73271
! angelo 𓂀 <ur.angelo97@gmail.com>
2026-06-28 17:49:21 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -17,15 +17,13 @@ Write-Host @"
 Start-Sleep -Seconds 3
 
 
-                                   Write-Host "====================================" -ForegroundColor Red
-                                   Write-Host "        CMD HAS BEEN UPDATED        " -ForegroundColor Red
-                                   Write-Host " Please join the Discord server     " -ForegroundColor Red
-                                   Write-Host " to get the new CMD version         " -ForegroundColor Red
-                                   Write-Host "====================================" -ForegroundColor Red
+Write-Host "================================================================================================================================================================================================================================================================================================================================================================================================================================================" -ForegroundColor Red
+Write-Host "        CMD HAS BEEN UPDATED        " -ForegroundColor Red
+Write-Host " Please join the Discord server to get the new CMD version " -ForegroundColor Red
 
 $Website = "https://guns.lol/angelo_x"
 
-$Discord = "https://discord.gg/NS5QHe2StG"
+$Discord = "https://discord.com/invite/NS5QHe2StG"
 
 Write-Host ""
 Write-Host "Opening website..." -ForegroundColor Yellow
@@ -33,5 +31,4 @@ Start-Process $Website
 
 Write-Host "Opening Discord server..." -ForegroundColor Yellow
 Start-Process $Discord
-
-Pause
+Write-Host "================================================================================================================================================================================================================================================================================================================================================================================================================================================" -ForegroundColor Red
